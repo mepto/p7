ab8b7ebe6c085369fa8c244e389732c617e019d7
mepto <natg.inf@gmail.com>
2022-01-18 01:00:26 +0100

    Update main script

--- main.py
+++ main.py
@@ -2,11 +2,21 @@ from datetime import timedelta
 from timeit import default_timer as timer
 
 from bruteforce import BruteForce
+from optimized import Wallet
+# from optimized import Optimised
+from utils import write_results
 
 if __name__ == '__main__':
+    print('Optimized started...')
+    opt_start = timer()
+    cost, wallet, profit = Wallet().get_max_profit()
+    write_results(cost, wallet, profit)
+    opt_end = timer()
+    print(f"Time spent: {timedelta(seconds=opt_end - opt_start)}")
+    print('-------------------------')
     print('BruteForce started...')
-    start = timer()
-    data = BruteForce()
-    maxed = data.best_item()
-    end = timer()
-    print(f"Time spent: {timedelta(seconds=end - start)}")
+    brute_start = timer()
+    cost, wallet, profit = BruteForce().best_item()
+    write_results(cost, wallet, profit)
+    brute_end = timer()
+    print(f"Time spent: {timedelta(seconds=brute_end - brute_start)}")

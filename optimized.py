30f603881a6ae18f4c6e6ef4e18e9a7083fddc11
mepto <natg.inf@gmail.com>
2022-06-15 10:16:08 +0200

    Make optimised file standalone

--- optimized.py
+++ optimized.py
@@ -1,20 +1,74 @@
-from constants import MAX_COST
-from menu import Menu
-from utils import CsvFile, timer
+import argparse
+import csv
+import time
+from datetime import timedelta
+from pathlib import Path
+
+MAX_COST = 500
+
+
+def timer(func):
+    def inner(*args, **kwargs):
+        start = time.time()
+        f = func(*args, **kwargs)
+        end = time.time()
+        print(f'Programme duration time: {timedelta(seconds=end - start)}')
+        return f
+    return inner
+
+
+class CsvFile:
+    """Read and store data."""
+    def __init__(self, dataset):
+        self.filepath = Path(dataset)
+        # Check if path provided is relevant
+        if not self.filepath.is_file():
+            raise FileExistsError('The path provided is not to a file or the file does not exist.')
+
+    def read(self) -> dict:
+        """ Read data from csv file """
+
+        all_data = []
+
+        with open(self.filepath, newline='') as csvfile:
+            dataset = csv.reader(csvfile, delimiter=',', quotechar='|')
+            next(dataset, None)  # skip the headers
+            skipped_actions = 0
+            for rows in dataset:
+                if float(rows[1]) > 0 and (float(rows[1]) * float(rows[2]) / 100) > 0:
+                    action = rows[0]
+                    cost = int(round(float(rows[1]), 2) * 100)
+                    profit = int(round(float(rows[2]), 2) * 100)
+                    actual_profit = int(round(float(rows[1]) * (float(rows[2]) / 100), 2) * 100)
+
+                    all_data.append(
+                        (
+                            action,
+                            cost,
+                            profit,
+                            actual_profit
+                        ),
+                    )
+                else:
+                    skipped_actions += 1
+
+        print(f'{skipped_actions} actions were skipped (action price or actual profit were negative).')
+
+        return list(reversed(sorted(all_data, key=lambda x: x[1])))
 
 
 class Wallet:
     """ Create optimised wallet for actions list """
 
-    def __init__(self, dataset=None):
+    def __init__(self, dataset: dict = None):
         """ Instantiate Wallet object """
-        self.dataset = CsvFile(tup=True, dataset=dataset).read()
-        Menu.actions_nb(len(self.dataset))
+        self.dataset = CsvFile(dataset).read()
+        print(f'Working with {len(self.dataset)} actions...')
 
     @timer
-    def get_best_wallet(self):
+    def get_best_wallet(self) -> tuple[float, tuple, float]:
         """
-        Generate matrix to get best items in wallet
+        Generate matrix to get best items in wallet.
         :return: cost as float, wallet as tuple, profit as float
         """
         actions = self.dataset
@@ -33,7 +87,7 @@ class Wallet:
                 else:
                     matrix[row][col] = matrix[row - 1][col]
 
-        # Retrieve beset actions based on sum
+        # Retrieve best actions based on sum
         current_capacity = capacity
         current_row = len(actions)
         actions_selection = []
@@ -58,3 +112,36 @@ class Wallet:
 
         # Return cost (refloated), wallet, profit (refloated)
         return float(cost / 100), wallet, float(profit / 100)
+
+
+def init_argparse() -> argparse.ArgumentParser:
+    """Parse arguments passed in command line"""
+    parser = argparse.ArgumentParser(
+        usage="%(prog)s [OPTION] [FILE]...",
+        description="Pass a full file path with which to make the best actions wallet."
+    )
+
+    parser.add_argument('file', nargs='*')
+    return parser
+
+
+if __name__ == '__main__':
+    # Get dataset selection
+    parser = init_argparse()
+    args = parser.parse_args()
+    # Check if quantity of args given is correct
+    if not args.file or len(args.file) != 1:
+        raise NotImplementedError("This programme requires 1 path to a file exactly to proceed.")
+
+    filepath = args.file[0]
+    print(f'Selected file: {filepath}')
+
+    cost, wallet, profit = Wallet(filepath).get_best_wallet()
+
+    # Display results in console
+    print('----------- Results -----------')
+    print(f'Total cost: {cost}')
+    print(f'Total profit:{profit}')
+    print('*** BEST WALLET: ***')
+    for action in sorted(wallet):
+        print(f'{action}')
